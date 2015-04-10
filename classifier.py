53151e344acdec969e04a9b8f248142d62d70b85
Mehdi Cherti <mehdi@cherti.name>
2015-04-10 17:04:05 +0200

    adaNeuralNet

--- classifier.py
+++ classifier.py
@@ -1,20 +1,34 @@
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
+
+from sklearn.base import BaseEstimator
  
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+from lasagne.easy import SimpleNeuralNet
+import numpy as np
+from sklearn.preprocessing import StandardScaler, Imputer
+from sklearn.pipeline import Pipeline
+import theano
  
 class Classifier(BaseEstimator):
  
     def __init__(self):
-        self.clf = Pipeline([
-            ('rf', AdaBoostClassifier(
-                base_estimator=RandomForestClassifier(
-                    max_depth=5, n_estimators=30),
-                n_estimators=10)
-             )
+
+	nnet = Pipeline([
+            ('imputer', Imputer()),
+            ('scaler', StandardScaler()),
+            ('neuralnet', SimpleNeuralNet(nb_hidden_list=[30],
+                                          max_nb_epochs=50,
+                                          batch_size=100,
+                                          learning_rate=1.)),
         ])
-	self.clf = AdaBoostClassifier(base_estimator=self.clf)
+	
+        self.clf = Pipeline([
+            ('rf', AdaBoostClassifier(base_estimator=nnet,
+				      n_estimators=10))
+        ])
  
     def fit(self, X, y):
         self.clf.fit(X, y)
